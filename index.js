2323e298f4722066575ac16272db892d50149268
ban hussein <banhussien97@gmail.com>
2019-09-29 23:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+<<<<<<< HEAD
 let totalBatteries =batteryBatches.reduce(function(battry,price)
 {
   
   return battry + price;
 },0);
+=======
+let totalBatteries =batteryBatches.reduce(Function(battry+price)
+{
+  
+  return battry+price
+}
+,0);
+>>>>>>> bca3294d8336ecc18ecf5ae594116667e37d6abe
  
